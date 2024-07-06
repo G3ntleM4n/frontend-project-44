3976574c09d5e4ab082d75699f33cb28bf4ea227
Sokolov Alexander <alexsokolov123654@mail.ru>
2024-07-06 19:07:40 +0300

    add "brain-prime" game at bin/brain-prime.js

---
+++ bin/brain-prime.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import {
+  randomNumber, answer, isEvenNumber, isRightAnswer, getUserNameAndGreet,
+  isPrimeNumber,
+} from '../src/index.js';
+
+let counterOfCorrectAnswers = 0;
+let isCorrect = true;
+
+const userName = getUserNameAndGreet();
+console.log('Answer "yes" if the number is prime, otherwise answer "no".');
+
+while (counterOfCorrectAnswers < 3) {
+  const questionNumber = randomNumber(1, 100);
+  console.log(`Question: ${questionNumber}`);
+  const userAnswer = answer().toLowerCase();
+
+  if (isPrimeNumber(questionNumber)) {
+    const rightAnswer = 'yes';
+    isCorrect = isRightAnswer(userAnswer, userName, rightAnswer);
+  } else if (!isPrimeNumber(questionNumber)) {
+    const rightAnswer = 'no';
+    isCorrect = isRightAnswer(userAnswer, userName, rightAnswer);
+  }
+
+  if (isCorrect) {
+    counterOfCorrectAnswers += 1;
+  } else {
+    break;
+  }
+}
+
+if (isCorrect) {
+  console.log(`Congratulations, ${userName}!`);
+}
