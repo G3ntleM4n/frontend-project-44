0f06aab8ae74d5f5d1e2061d5f1cb07298566e00
Sokolov Alexander <alexsokolov123654@mail.ru>
2024-07-09 17:47:29 +0300

    add module with shared method which opens/plays the game

---
+++ src/shared-games-logic-module.js
@@ -0,0 +1,76 @@
+import brainCalcLogic from './games/brain-calc-module.js';
+import brainEvenLogic from './games/brain-even-module.js';
+import brainGcdLogic from './games/brain-gcd-module.js';
+import brainPrimeLogic from './games/brain-prime-module.js';
+import brainProgressionLogic from './games/brain-progression-module.js';
+import { getUserNameAndGreet } from './index.js';
+
+export const chooseGame = (gameNumber, isCorrect, userName) => {
+  let isAnswerCorrect = isCorrect;
+  switch (gameNumber) {
+    case 1:
+      isAnswerCorrect = brainEvenLogic(isCorrect, userName);
+      break;
+    case 2:
+      isAnswerCorrect = brainCalcLogic(isCorrect, userName);
+      break;
+    case 3:
+      isAnswerCorrect = brainGcdLogic(isCorrect, userName);
+      break;
+    case 4:
+      isAnswerCorrect = brainProgressionLogic(isCorrect, userName);
+      break;
+    case 5:
+      isAnswerCorrect = brainPrimeLogic(isCorrect, userName);
+      break;
+    default:
+      console.log('Game does not exist!');
+      break;
+  }
+  return isAnswerCorrect;
+};
+
+export const chooseGamePhrase = (gameNumber) => {
+  switch (gameNumber) {
+    case 1:
+      console.log('Answer "yes" if the number is even, otherwise answer "no".');
+      break;
+    case 2:
+      console.log('What is the result of the expression?');
+      break;
+    case 3:
+      console.log('Find the greatest common divisor of given numbers.');
+      break;
+    case 4:
+      console.log('What number is missing in the progression?');
+      break;
+    case 5:
+      console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+      break;
+    default:
+      console.log('Game does not exist!');
+      break;
+  }
+};
+
+export const playGame = (gameNumber) => {
+  let counterOfCorrectAnswers = 0;
+  let isCorrect = true;
+
+  const userName = getUserNameAndGreet();
+  chooseGamePhrase(gameNumber);
+
+  while (counterOfCorrectAnswers < 3) {
+    isCorrect = chooseGame(gameNumber, isCorrect, userName);
+
+    if (isCorrect) {
+      counterOfCorrectAnswers += 1;
+    } else {
+      break;
+    }
+  }
+
+  if (isCorrect) {
+    console.log(`Congratulations, ${userName}!`);
+  }
+};
