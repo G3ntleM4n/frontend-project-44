b169630ffdf87570f82b145e621bc523f5cc8806
Sokolov Alexander <alexsokolov123654@mail.ru>
2024-07-11 12:29:02 +0300

    refactor brain-prime game

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import { playGame } from '../src/shared-games-logic-module.js';
+import playPrime from '../src/games/brain-prime-module.js';
 
-playGame(5);
+playPrime();
---
+++ src/games/brain-prime-module.js
@@ -0,0 +1,22 @@
+import {
+  isPrimeNumber, randomNumber,
+} from '../index.js';
+import { playGame } from '../shared-games-logic-module.js';
+
+export const brainPrimeLogic = () => {
+  const question = randomNumber(1, 100);
+  const isPrime = isPrimeNumber(question);
+  let correctAnswer = '';
+
+  if (isPrime) {
+    correctAnswer = 'yes';
+  } else {
+    correctAnswer = 'no';
+  }
+
+  return [question, correctAnswer];
+};
+
+export default () => {
+  playGame('Answer "yes" if given number is prime. Otherwise answer "no".', brainPrimeLogic);
+};
