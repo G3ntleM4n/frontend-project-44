a71393d90d01dc04261b2773ed909c9a3f8d6f69
Sokolov Alexander <alexsokolov123654@mail.ru>
2024-07-17 13:53:03 +0300

    refactor main method at src/shared-games-logic-module.js

--- src/shared-games-logic-module.js
+++ src/shared-games-logic-module.js
@@ -1,22 +1,23 @@
 import { getAnswer, getUserNameAndGreet, isRightAnswer } from './index.js';
 
 const playGame = (description, gameData) => {
-  let counterOfCorrectAnswers = 0;
+  const maxRounds = 3;
   let isCorrect = true;
   const userName = getUserNameAndGreet();
   console.log(description);
 
-  while (counterOfCorrectAnswers < 3) {
+  for (let i = 0; i < maxRounds; i += 1) {
     const [question, correctAnswer] = gameData();
     console.log(`Question: ${question}`);
+    console.log(`answer: ${correctAnswer}`);
     const userAnswer = getAnswer();
     isCorrect = isRightAnswer(userAnswer, userName, correctAnswer);
 
-    if (isCorrect) {
-      counterOfCorrectAnswers += 1;
-    } else {
+    if (!isCorrect) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`);
       break;
     }
+    console.log('Correct!');
   }
 
   if (isCorrect) {
