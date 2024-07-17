2be14e5b2d6380790ec3acd40cc333182ce76748
Sokolov Alexander <alexsokolov123654@mail.ru>
2024-07-17 13:45:22 +0300

    refactor methods at src/games/brain-progression-module.js

--- src/games/brain-progression-module.js
+++ src/games/brain-progression-module.js
@@ -1,17 +1,36 @@
-import {
-  makeFinalArray, makeRandomArray,
-} from '../index.js';
+import { randomNumber } from '../index.js';
 import playGame from '../shared-games-logic-module.js';
 
-export const brainProgressionLogic = () => {
+export const makeRandomArray = (lengthOfArray, arrayStep) => {
   const array = [];
-  const randomArray = makeRandomArray(array);
-  const [skippedElement, finalArray] = makeFinalArray(randomArray);
+  let buffer = 0;
 
-  const question = finalArray.join(' ');
-  finalArray.splice(0, finalArray.length); // cleans array
+  for (let i = 0; i < lengthOfArray; i += 1) {
+    buffer += arrayStep;
+    array.push(buffer);
+  }
 
-  return [question, skippedElement];
+  return array;
+};
+
+export const removeArrayElement = (randomArray) => {
+  const array = randomArray;
+  const removedArrayStep = randomNumber(0, array.length - 1);
+  const removedElement = array[removedArrayStep];
+
+  return [removedElement, removedArrayStep];
+};
+
+export const brainProgressionLogic = () => {
+  const lengthOfArray = randomNumber(5, 10);
+  const arrayStep = randomNumber(1, 10);
+  const array = makeRandomArray(lengthOfArray, arrayStep);
+  const [correctAnswer, removedArrayStep] = removeArrayElement(array);
+
+  array[removedArrayStep] = '..';
+  const question = array.join(' ');
+
+  return [question, correctAnswer];
 };
 
 export default () => {
